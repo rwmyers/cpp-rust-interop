00010505a43ad6d5329c42e5818bf1c9e6890d92
Russell Myers <mezner@russellmyers.com>
2024-05-05 20:56:41 -0400

    Build a few Android versions.

--- build.sh
+++ build.sh
@@ -9,8 +9,26 @@ function proto-build() {
     fi
 
     build_dir=$git_root/build
+
     mkdir -p $build_dir
-    pushd $build_dir
-    cmake .. && cmake --build .
-    popd
+
+    SYSTEM_NAME="Android"
+    SYSTEM_VERSION=21
+    ANDROID_NDK="/home/mezner/Android/android-ndk-r26d"
+    targets=("armeabi-v7a" "x86" "arm64-v8a")
+    for target in ${targets}; do
+        target_dir=${build_dir}/${target}
+        mkdir -p $target_dir
+        pushd $target_dir
+        cmake \
+           -DCMAKE_SYSTEM_NAME=$SYSTEM_NAME \
+           -DCMAKE_SYSTEM_VERSION=$SYSTEM_VERSION \
+           -DCMAKE_ANDROID_ARCH_ABI=$target \
+           -DCMAKE_ANDROID_NDK=$ANDROID_NDK \
+           $git_root && \
+        cmake --build .
+
+        popd
+
+    done
 }
